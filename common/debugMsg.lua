1624bff5423d0684a497937d2f6c812bdd5fa90f
hs-sh.net <info@hs-sh.net>
2016-10-06 15:23:46 +0900

    デバッグメッセージ系関数を別ファイルに移動

--- common/debugMsg.lua
+++ common/debugMsg.lua
@@ -13,24 +13,27 @@ Msg = function(str)
         print("\x1b[47m\x1b[30m".."Info:".."\x1b[49m\x1b[39m "..str)
     end
 end
+
 Load = function(str)
     if debugmode then
         print("\x1b[47m\x1b[30m".."Load:".."\x1b[49m\x1b[39m "..str)
     end
 end
+
 Exec = function(str)
     if debugmode then
         print("\x1b[47m\x1b[30m".."Exec:".."\x1b[49m\x1b[39m "..str)
     end
 end
+
 Complate  = function(str)
     if debugmode then
         print("\x1b[47m\x1b[30m".."Comp:".."\x1b[49m\x1b[39m "..str)
     end
 end
+
 ComplateOW = function(str)
     if debugmode then
         io.write("\r\x1b[47m\x1b[30m".."Comp:".."\x1b[49m\x1b[39m "..str)
     end
 end
-
