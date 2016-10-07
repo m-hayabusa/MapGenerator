44a626e2ed368f25728104db221f3c69dd51394c
hs-sh.net <info@hs-sh.net>
2016-10-07 09:16:25 +0900

    複数のスムージング処理を実装

--- generator/smoothing.lua
+++ generator/smoothing.lua
@@ -4,38 +4,47 @@ smoothing = function()
   for i = 1, World.SmoothingTime do
     for i_x = 1, World.size do
       for i_y = 1, World.size do
-        ZeroHeightCellCount = 0
-        if i_x - 1 < 1 or World.Map[i_x - 1][i_y].Type.Height == CellType.Sea.Height then
-          ZeroHeightCellCount = ZeroHeightCellCount + 1
-        end
-        if i_x + 1 > World.size or World.Map[i_x + 1][i_y].Type.Height == CellType.Sea.Height then
-          ZeroHeightCellCount = ZeroHeightCellCount + 1
-        end
-        if i_y - 1 < 1 or World.Map[i_x][i_y - 1].Type.Height == CellType.Sea.Height then
-          ZeroHeightCellCount = ZeroHeightCellCount + 1
-        end
-        if i_y + 1 > World.size or World.Map[i_x][i_y + 1].Type.Height == CellType.Sea.Height then
-          ZeroHeightCellCount = ZeroHeightCellCount + 1
-        end
-        --[[ --上下左右ではなく3x3の正方形で実行してみた → かなり偏ってしまう
-        for j_x = i_x-1,i_x+1 do
-          for j_y = i_y-1, i_y+1 do
-            if j_x < 1 or j_x > World.size or j_y < 1 or j_y > World.size or World.Map[j_x][j_y].Type.Height == CellType.Sea.Height then
-              ZeroHeightCellCount = ZeroHeightCellCount + 0.75
+        local ZeroHeightCellCount = 0
+
+        if World.SmoothingMode == 1 or World.SmoothingMode == 3 then
+            if i_x - 1 < 1 or World.Map[i_x - 1][i_y].Type.Height == CellType.Sea.Height then
+              ZeroHeightCellCount = ZeroHeightCellCount + 1
             end
-          end
-        end]]
-        --[[--そのセルを挟んで左右が同じTypeの場合､左右に近づける
-        if not (i_x - 1 < 1 or i_x + 1 > World.size) then
-          if World.Map[i_x+1][i_y].Type == World.Map[i_x-1][i_y].Type and World.Map[i_x][i_y].Type ~= World.Map[i_x-1][i_y].Type then
-            ZeroHeightCellCount = ZeroHeightCellCount + 0.1
-          end
+            if i_x + 1 > World.size or World.Map[i_x + 1][i_y].Type.Height == CellType.Sea.Height then
+              ZeroHeightCellCount = ZeroHeightCellCount + 1
+            end
+            if i_y - 1 < 1 or World.Map[i_x][i_y - 1].Type.Height == CellType.Sea.Height then
+              ZeroHeightCellCount = ZeroHeightCellCount + 1
+            end
+            if i_y + 1 > World.size or World.Map[i_x][i_y + 1].Type.Height == CellType.Sea.Height then
+              ZeroHeightCellCount = ZeroHeightCellCount + 1
+            end
         end
-        if not (i_y - 1 < 1 or i_y + 1 > World.size) then
-          if World.Map[i_x][i_y+1].Type == World.Map[i_x][i_y-1].Type and World.Map[i_x][i_y].Type ~= World.Map[i_x][i_y-1].Type then
-            ZeroHeightCellCount = ZeroHeightCellCount + 0.1
-          end
-        end]]
+
+        if World.SmoothingMode == 2 or World.SmoothingMode == 4 then
+            --上下左右ではなく3x3の正方形
+            for j_x = i_x-1,i_x+1 do
+              for j_y = i_y-1, i_y+1 do
+                if j_x < 1 or j_x > World.size or j_y < 1 or j_y > World.size or World.Map[j_x][j_y].Type.Height == CellType.Sea.Height then
+                  ZeroHeightCellCount = ZeroHeightCellCount + 0.75
+                end
+              end
+            end
+        end
+
+        if World.SmoothingMode == 3 or World.SmoothingMode == 4 then
+            --そのセルを挟んで左右が同じTypeの場合､左右に近づける
+            if not (i_x - 1 < 1 or i_x + 1 > World.size) then
+              if World.Map[i_x+1][i_y].Type == World.Map[i_x-1][i_y].Type and World.Map[i_x][i_y].Type ~= World.Map[i_x-1][i_y].Type then
+                ZeroHeightCellCount = ZeroHeightCellCount + 0.1
+              end
+            end
+            if not (i_y - 1 < 1 or i_y + 1 > World.size) then
+              if World.Map[i_x][i_y+1].Type == World.Map[i_x][i_y-1].Type and World.Map[i_x][i_y].Type ~= World.Map[i_x][i_y-1].Type then
+                ZeroHeightCellCount = ZeroHeightCellCount + 0.1
+              end
+            end
+        end
 
         World.Map[i_x][i_y].Type = randomizer(ZeroHeightCellCount /4 *World.raitoZeroHeightCellCount, CellType.Sea, CellType.Land)
       end
